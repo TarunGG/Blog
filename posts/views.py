b9b41bf3d3f8cf305563ec34539b765e54389a9d
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-03-26 18:55:36 +0530

    minor changes

--- posts/views.py
+++ posts/views.py
@@ -16,12 +16,13 @@ def index (request) :
         }
         
         return JsonResponse(routes)
+    else:
+        return JsonResponse({"success": "false", "error":"Wrong method use 'GET' method for this enpoint"})
     
 @csrf_exempt
 def create_post(request):
     if request.method == 'POST':
         posts = loads(request.body)
-        
         rsp_post = []
         for instance in posts['posts']:
             post = Posts.objects.create(
@@ -31,14 +32,15 @@ def create_post(request):
                 last_updated = date.today(),
                 username = instance.get("username",None)
             )
-            
             data = {
                 "id": post.id,
-                "title": post.title
+                "title": post.title,
+                "status": "Post created"
             }
-            
             rsp_post.append(data)
         return JsonResponse({"success":"true",'posts': rsp_post})
+    else:
+        return JsonResponse({"success":"false","error":"Wrong request method use 'POST' method for this endpoint"})
 
 @csrf_exempt
 def update_post(request, *args, **kwargs):
@@ -60,14 +62,13 @@ def update_post(request, *args, **kwargs):
         
         if username != None:
             post.username = username
-        
         post.save()
-        
-        return JsonResponse({"success":"true","something":"something"})
+        return JsonResponse({"success":"true","respons":"Post updated successfully"})
+    else:
+        return JsonResponse({"success":"false","error":"Wrong method, use 'PUT' method for this endpoint"})
 
 def all_post(request):
     if request.method == "GET":
-        
         posts = Posts.objects.all().order_by("last_updated")
         post_list = []
         for post in posts:
@@ -79,14 +80,14 @@ def all_post(request):
             data['created at'] = post.created_at
             data['last updated'] = post.last_updated
             post_list.append(data)
-        
         return JsonResponse({'posts':post_list})
+    else:
+        return JsonResponse({"success":"true","error":"Wrong request method use 'GET' method for this endpoint"})
 
 def sngl_post(request, *args, **kwargs):
     if request.method == "GET":
         id = kwargs['id']
         post = Posts.objects.get(id=id)
-        
         data = {
             "id": post.id,
             "title": post.title,
@@ -95,13 +96,23 @@ def sngl_post(request, *args, **kwargs):
             "created at": post.created_at,
             "last updated": post.last_updated
         }
-        
         return JsonResponse({"post":data})
+    else :
+        return JsonResponse({"success":"false","error":"use GET method to request for current path"})
 
 @csrf_exempt
 def delete_post(request,*args, **kwargs):
     if request.method == "DELETE":
         id = kwargs['id']
-        post = Posts.objects.get(id=id)
+        if id != None:
+            try:
+                post = Posts.objects.get(id=id)
+            except :
+                return JsonResponse({"success":"false","error":"Post does not exist"})
+        else:
+            return JsonResponse({"success":"false","error":"no id provided"})
+        
         post.delete()
-        return JsonResponse({"success":"true"})
+        return JsonResponse({"success":"true","status":"post with id " + str(id) + " successfully deleted"})
+    else :
+        return JsonResponse({"success":"flase","error":"Wrong request method use 'DELETE' method for this endpoint"})
