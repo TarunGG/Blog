df80457f79b4ec0e41be427eb48f9f08ef09b976
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-03-26 12:48:12 +0530

    feat: created home route response and posts model and added gitignore file

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=200,null=False)
+    description = models.TextField()
+    created_at = models.DateField(editable=False)
+    last_updated = models.DateField()
+    username = models.CharField(max_length=100,default=None) 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Posts
 
 # Create your views here.
+
+def index (request) :
+    if request.method == "GET":
+        
+        routes = {
+            'routes':[
+                {
+                    'route': '/',
+                    'method': 'GET',
+                    'response': 'returns all available routes'
+                },
+                {
+                    'route': '/all/',
+                    'method': 'GET',
+                    'response': 'returns all posts available in database'
+                    
+                },
+                {
+                    'route': '/all/id',
+                    'method': 'GET',
+                    'response': 'returns the post with provided id'
+                    
+                },
+                {
+                    'route': '/create/',
+                    'method': 'POST',
+                    'parameter': {
+                        'title': 'maximum limit of 200 characters (required)',
+                        'description': 'Text field (required)',
+                        'created by': 'name of the user 100 characters limit (optional)'
+                    },
+                    'response': 'creates a post with the provided title and description'
+                },
+                {
+                    'route': '/create/id',
+                    'method': 'PATCH',
+                    'usage': 'to edit the post with provided id but atleast one parameted is required',
+                    'parameter': {
+                        'title': 'new title with maximum limit of 200 characters (optional)',
+                        'description': 'new description text field (optional)',
+                        'created by': 'name of the user (optional)'
+                    },
+                    'response': 'edit the post accordingly to provided parameters'
+                },
+                {
+                    'route': '/remove/id',
+                    'method': 'DELETE',
+                    'usage': 'to delete the post with provided id',
+                    'response': 'deletes the post with provided id from database'
+                },
+            ]
+        }
+        
+        return JsonResponse(routes)
