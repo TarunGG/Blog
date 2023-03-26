a5a53de3eed759020d7a87602f0553cdb5ec3410
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-03-26 17:43:37 +0530

    minor changes

--- posts/views.py
+++ posts/views.py
@@ -10,52 +10,8 @@ from django.views.decorators.csrf import csrf_exempt
 def index (request) :
     if request.method == "GET":
         
-        routes = {
-            'routes':[
-                {
-                    'route': '/',
-                    'method': 'GET',
-                    'response': 'returns all available routes'
-                },
-                {
-                    'route': '/all/',
-                    'method': 'GET',
-                    'response': 'returns all posts available in database'
-                    
-                },
-                {
-                    'route': '/all/id',
-                    'method': 'GET',
-                    'response': 'returns the post with provided id'
-                    
-                },
-                {
-                    'route': '/create/',
-                    'method': 'POST',
-                    'parameter': {
-                        'title': 'maximum limit of 200 characters (required)',
-                        'description': 'Text field (required)',
-                        'created by': 'name of the user 100 characters limit (optional)'
-                    },
-                    'response': 'creates a post with the provided title and description'
-                },
-                {
-                    'route': '/create/id',
-                    'method': 'PATCH',
-                    'usage': 'to edit the post with provided id but atleast one parameted is required',
-                    'parameter': {
-                        'title': 'new title with maximum limit of 200 characters (optional)',
-                        'description': 'new description text field (optional)',
-                        'created by': 'name of the user (optional)'
-                    },
-                    'response': 'edit the post accordingly to provided parameters'
-                },
-                {
-                    'route': '/remove/id',
-                    'method': 'DELETE',
-                    'usage': 'to delete the post with provided id',
-                    'response': 'deletes the post with provided id from database'
-                },
+        routes = { "routes": [
+                "/","/create","/create/<id>","/all","/all/<id>", "/delete/<id>"
             ]
         }
         
